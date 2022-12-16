d35135a61e918370198e1f9a10e50e6278f1ca10
joseph-hellerstein <joseph.hellerstein@gmail.com>
2022-12-16 08:34:42 -0800

    Updates for toml file

--- distrib.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Create a new distribution. Runs twine to upload the file.
-if [ -d dist ] 
-then
-    rm -rf $DIR
-fi
-#python setup.py sdist
-python -m build
-twine upload dist/*.*
--- fitterpp/_version.py
+++ fitterpp/_version.py
@@ -1,1 +1,13 @@
-__version__ = "0.0.6"
+"""Finds the version in pyproject.toml"""
+
+import tomli
+import os
+
+DIR = os.path.dirname(os.path.abspath(__file__))
+DIR = os.path.dirname(DIR)
+PATH = os.path.join(DIR, "pyproject.toml")
+
+with open(PATH, "rb") as f:
+    data = tomli.load(f)
+
+__version__ = data["project"]["version"]
